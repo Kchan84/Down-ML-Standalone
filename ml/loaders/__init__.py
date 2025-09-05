ca1acd4a0ae54f07c9a1511a70904ad1d3768a27
Kevin Chan <142963635+Kchan84@users.noreply.github.com>
2025-09-04 19:36:57 -0700

    Delete ml/loaders directory

--- ml/loaders/__init__.py
+++
@@ -1,8 +0,0 @@
-# Loaders package for Yelp data processing
-from .business import load_business_features
-from .reviews import load_review_features
-
-__all__ = [
-    'load_business_features', 
-    'load_review_features'
-]
