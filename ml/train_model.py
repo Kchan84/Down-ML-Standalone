a69055ce395b5759e5e95ea5f181d22a0bf3ab84
Kevin Chan <142963635+Kchan84@users.noreply.github.com>
2025-09-04 19:42:37 -0700

    Delete ml/train_model.py

--- ml/train_model.py
+++
@@ -1,4 +0,0 @@
-# User query: "quiet place for conversation"
-# TF-IDF finds reviews with similar words:
-# "quiet", "peaceful", "conversation", "intimate", "serene"
-# Recommends businesses with high similarity scores
